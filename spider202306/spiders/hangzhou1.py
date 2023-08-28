250551a5912098e28e1bda0e4a200f5fab6bb527
Lance <75961930+Lance-Owen@users.noreply.github.com>
2023-08-28 19:30:15 +0800

    '代码提交'

---
+++ spider202306/spiders/hangzhou1.py
@@ -0,0 +1,5 @@
+# import pandas as pd
+# df = pd.read_csv('杭州市开标项目信息2023-08-25.csv',encoding='gbk')
+# df = df.sort_values(by='投标单位数量').drop_duplicates(subset=['项目编号','project_type'],keep='first')
+# df.to_csv('杭州市开标项目信息2023-08-25.csv', index_label=False)
+
--- spider202306/spiders/hangzhou2.py
+++ spider202306/spiders/hangzhou2.py
@@ -62,16 +62,30 @@ def get_open_project(url, id):
         return '',2, tender_num
     else:
         return '', 0, tender_num
+def get_project(url):
+    payload = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Content-Type': 'text/html; charset=utf-8'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    html = etree.HTML(response.text)
+    try:
+        return html.xpath('//table[2]/tbody/tr[11]/td[2]/p/span[1]')[0].text
+    except:
+        print(url)
+        return ''
 
 data = pd.DataFrame()
 # 遍历所有翻页数据
-for page_num in range(1,106):
+for page_num in range(1,98):
     print(f"当前获取第 {page_num} 页数据".center(100,"*"))
     # 获取当前时间戳
     millisecond_timestamp = round(time.time() * 1000)
     # 拼接获取每页项目信息的请求头和表单数据
     url = "https://ggzy.hzctc.hangzhou.gov.cn/SecondPage/GetNotice"
-    payload = f"area=&afficheType=486&IsToday=&title=&proID=&number=&_search=false&nd={millisecond_timestamp}&rows=10&page={page_num}&sidx=PublishStartTime&sord=desc"
+    payload = f"area=&afficheType=22&IsToday=&title=&proID=&number=&_search=false&nd={millisecond_timestamp}&rows=10&page={page_num}&sidx=PublishStartTime&sord=desc"
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Cookie': 'HZSESSIONID=c7693883-1f26-4dc1-a90d-4c917203153d; SL_G_WPT_TO=zh-CN; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; ASP.NET_SessionId=x0poiuhv0ja5y3hs54roksej',
@@ -85,12 +99,13 @@ for page_num in range(1,106):
     # 遍历项目，拼接项目请求地址，获取项目网页信息，xpath获取参数抽取信息
     for project in project_list:
         ID = project['ID']
-        TenderID = project['TenderID']
-        open_project_url = f"https://ggzy.hzctc.hangzhou.gov.cn/OpenBidRecord/Index?id={ID}&tenderID={TenderID}&ModuleID=486"
-        extract_url,url_type,tender_num = get_open_project(open_project_url, ID)
+        # TenderID = project['TenderID']
+        project_url = f"https://ggzy.hzctc.hangzhou.gov.cn/AfficheShow/Home?AfficheID={ID}&IsInner=0&IsHistory=&ModuleID=22"
+        print(project_url)
+        _ = get_project(project_url)
         print(open_project_url, extract_url, url_type,tender_num)
-        df = pd.DataFrame(data=[[ID,TenderID,project['TenderNo'],project['TenderName'],open_project_url,extract_url,url_type,project['ProTypeStr'],tender_num]], columns=['id','tenderid','项目编号','项目名称','开标网址','参数抽取网址','参数抽取类型','project_type','投标单位数量'])
-        data = pd.concat([data, df])
-        time.sleep(random.randint(1,5))
+        # df = pd.DataFrame(data=[[ID,TenderID,project['TenderNo'],project['TenderName'],open_project_url,extract_url,url_type,project['ProTypeStr'],tender_num]], columns=['id','tenderid','项目编号','项目名称','开标网址','参数抽取网址','参数抽取类型','project_type','投标单位数量'])
+        # data = pd.concat([data, df])
+        # time.sleep(random.randint(1,5))
 # print(data)
 data.to_csv('杭州市开标项目信息2023-08-25.csv', index=False)
