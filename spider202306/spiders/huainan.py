17098285b96505cc62f6875d65e11c1e374502aa
Lance <75961930+Lance-Owen@users.noreply.github.com>
2023-09-28 17:21:37 +0800

    '代码更新'

--- spider202306/spiders/huainan.py
+++ spider202306/spiders/huainan.py
@@ -1,36 +1,51 @@
 import requests
-import datetime
+import pandas as pd
+import re
+import time
+import random
+def request_text(url):
+    '''
+    get请求页面信息
+    :param url:
+    :return:
+    '''
+    # time.sleep(random.random())
+    time.sleep(random.randint(1,4))
+    payload = {}
+    headers = {}
 
-url = "http://jy.ggj.huainan.gov.cn/jyxx/012001/012001004/secondPageJYXX.html"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    #
+    html_text = response.text
+    return html_text
+def get_bidders_list(text):
+    '''
+    获取开标记录中投标单位报价信息，返回为dataframe，并且去除报价为空的数据
+    '''
+    del_label = re.compile(r'<[^>]+>', re.S)
+    re_ul = re.compile("<ul class='tabel'>([\s\S]*?)</ul>", re.IGNORECASE)
+    re_li = re.compile('<li[^>]*>[\s\S]*?<\/li>', re.IGNORECASE)
+    re_span = re.compile('<span[^>]*>[\s\S]*?</span>', re.IGNORECASE)
 
-payload = {}
-headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-}
+    tables = re_ul.findall(text)
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    table_text = []
+    for row in re_li.findall(tables[0]):
+        table_text.append([])
+        for col in re_span.findall(row):
+            s = del_label.sub("", re.sub("\s", '', col))
+            if re.findall('投标单位', s) and len(s) <= 10:
+                s = '投标单位名称'
+                table_text = table_text[-1:]
+            table_text[-1].append(s)
+        if not table_text[-1]:
+            table_text = table_text[:-1]
+    df = pd.DataFrame(table_text[1:], columns=['序号', '投标单位名称', '投标报价', '工期'])
+    df = df[df['投标报价'] != '']
+    return df
 
-html = response.text
-# print(html)
-
-from lxml import etree
-element = etree.HTML(html)
-nodes = element.xpath("//li[@class='ewb-list-node clearfix']")
-for node in nodes:
-    project_name = node.xpath('a/text()')[0]
-    print(project_name)
-
-    # print(node)
-    bid_url = f"http://jy.ggj.huainan.gov.cn/{node.xpath('a/@href')[0]}"
-    # print(url)
-    date = node.xpath('span')[0].text
-    yes = datetime.datetime.now().date() + datetime.timedelta(days=-1)
-    if datetime.datetime.strptime(date,'%Y-%m-%d').date() == yes:
-        response = requests.request("GET", bid_url, headers=headers, data=payload)
-        bid_html = response.text
-        bid_element = etree.HTML(bid_html)
-        bid_price = bid_element.xpath("//tr[2]/td[5]/p/span")[0].text
-        print(bid_price)
-        tender_url = bid_element.xpath("//li[@id='012001001']/a/@href")
-        print(tender_url)
+def get_extract_parameters(text):
+    extract_parameters = re.findall('现场抽取系数.*?>(.*?)<',text)[0]
+    if str.strip(extract_parameters) in ['0','/','\\']:
+        return ''
+    return str.strip(extract_parameters)
--- spider202306/spiders/huainan_open.py
+++ spider202306/spiders/huainan_open.py
@@ -1,6 +1,5 @@
-import requests
 import pandas as pd
-import re
+from huainan import *
 # url = "http://jy.ggj.huainan.gov.cn/jyxx/012001/012001012/012001012001/20230830/f540c6ac-e5e7-4306-bbfc-edc92ea65bab-1KB.html"
 """
 淮南市开标记录表下载
@@ -12,41 +11,13 @@ import re
     5、保存文件为：“淮南项目开标记录.xlsx”文件
 """
 
-
-
-url = input("url:")
-payload = {}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-#
-html_text = response.text
-
-del_label = re.compile(r'<[^>]+>', re.S)
-re_ul = re.compile("<ul class='tabel'>([\s\S]*?)</ul>", re.IGNORECASE)
-re_li = re.compile('<li[^>]*>[\s\S]*?<\/li>', re.IGNORECASE)
-re_span = re.compile('<span[^>]*>[\s\S]*?</span>', re.IGNORECASE)
-
-tables = re_ul.findall(html_text)
-
-table_text = []
-for row in re_li.findall(tables[0]):
-    table_text.append([])
-    for col in re_span.findall(row):
-        s = del_label.sub("", re.sub("\s", '', col))
-        if re.findall('投标单位', s) and len(s) <= 10:
-            s = '投标单位名称'
-            table_text = table_text[-1:]
-        table_text[-1].append(s)
-    if not table_text[-1]:
-        table_text = table_text[:-1]
-df = pd.DataFrame(table_text[1:],columns=['序号','投标单位名称','投标报价','工期'])
-df = df[df['投标报价']!= '']
-
-
-
 # zbkzj = 14789847.96
-zbkzj = float(input('招标控制价：'))
-df['下浮率'] = df['投标报价'].apply(lambda x:100-100*float(x)/zbkzj)
-df.sort_values(by=['下浮率'],inplace=True)
-df.to_excel('淮南项目开标记录.xlsx',index=False)
+if __name__ == '__main__':
+    url = input("url:")
+    zbkzj = float(input('招标控制价：'))
+    text = request_text(url=url)
+    df = get_bidders_list(text)
+    df = df[df['投标报价'] != '']
+    df['下浮率'] = df['投标报价'].apply(lambda x:100-100*float(x)/zbkzj)
+    df.sort_values(by=['下浮率'],inplace=True)
+    df.to_excel('淮南项目开标记录.xlsx',index=False)
